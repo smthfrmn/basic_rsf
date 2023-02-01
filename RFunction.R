4699c4165e1896f39f5ba1b4cf19559540b72645
Nilanjan Chatterjee <55828175+nilanjanchatterjee@users.noreply.github.com>
2023-02-01 12:11:27 +0530

    Initial commit

---
+++ RFunction.R
@@ -0,0 +1,10 @@
+library('move')
+
+## The parameter "data" is reserved for the data object passed on from the previous app
+
+## to display messages to the user in the log file of the App in MoveApps one can use the function from the logger.R file: 
+# logger.fatal(), logger.error(), logger.warn(), logger.info(), logger.debug(), logger.trace()
+
+rFunction = function(data, year) {
+  data[lubridate::year(data@timestamps) == year]
+}
